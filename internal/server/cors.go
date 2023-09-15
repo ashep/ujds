5c082060af970be39df326a3f4c99bd04c4d45c4
Oleksandr Shepetko <a@shepetko.com>
2023-09-15 13:42:29 +0300

    Add simple CORS support #25

--- internal/server/cors.go
+++ internal/server/cors.go
@@ -6,11 +6,12 @@ import (
 
 func cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Headers", "*")
+		w.Header().Add("Access-Control-Allow-Methods", "*")
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Expose-Headers", "*")
+
 		if r.Method == http.MethodOptions {
-			w.Header().Add("Access-Control-Allow-Headers", "*")
-			w.Header().Add("Access-Control-Allow-Methods", "*")
-			w.Header().Add("Access-Control-Allow-Origin", "*")
-			w.Header().Add("Access-Control-Expose-Headers", "*")
 			w.WriteHeader(http.StatusNoContent)
 			return
 		}
